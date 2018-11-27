cb2f3a89a45f818b9100c4fa1591d889dbc802af
Matteo Franceschi <mfranceschi94@gmail.com>
2018-11-28 00:19:51 +0100

    add support to travis

--- beepbeep/dataservice/views/home.py
+++ beepbeep/dataservice/views/home.py
@@ -1,6 +1,5 @@
-from flask import Blueprint
+from flask import Blueprint, redirect
 import os
-from flakon.util import send_request
 
 static_file_dir = os.path.dirname(os.path.realpath(__file__))
 home = Blueprint('home', __name__)
@@ -8,6 +7,4 @@ home = Blueprint('home', __name__)
 
 @home.route('/')
 def render_static():
-    res = send_request('http://127.0.0.1:5002/user/1', 'PUT', {'id': 1, 'strava_token': 'ciao'})
-    print(res)
-    return res.text
+    return redirect('/api/doc')
